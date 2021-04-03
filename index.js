b6e6195b9f6d606d363bb94cb912514f71f79112
Shim MunSeong <Puterism.K@gmail.com>
2021-04-03 11:44:12 +0900

    fix: package.json에 start script 추가

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-import express from "express";
-import path from "path";
+import express from 'express';
+import path from 'path';
 const __dirname = path.resolve();
 
 const app = express();
-app.use("/", express.static(__dirname + "/dist"));
+app.use('/', express.static(__dirname + '/dist'));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/dist/index.html"));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
-app.listen(5000, () => {
-  console.log("serve on http://localhost:5000");
-});
+app.listen(process.env.PORT || 5000);
